24840a64041b4d222af34d0cccc06bf1d9ea46cc
hesrayilmaz <hesra.yilmazz@gmail.com>
2022-09-09 01:33:14 +0300

    add score script

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public static int score;
+
+    private void Start()
+    {
+        score = 0;
+    }
+    private void Update()
+    {
+        scoreText.text = "" + score;
+    }
+
+    public void IncreaseScore(int point)
+    {
+        score += point;
+    }
+    public void DecreaseScore(int point)
+    {
+        score -= point;
+    }
+}
