3dcca7842e92f5f35dbd55916bc78487788f7e6a
hesrayilmaz <hesra.yilmazz@gmail.com>
2022-09-09 00:36:20 +0300

    create tile3

---
+++ Assets/Scripts/HookController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HookController : MonoBehaviour
+{
+    private Vector3 hookPos, targetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hookPos = transform.position;
+        targetPos = hookPos - new Vector3(0, 30f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.DOMove(targetPos, 10f);
+    }
+}
